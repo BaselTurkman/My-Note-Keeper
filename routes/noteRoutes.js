f984b6f732e682bcebb6fc8bf44ae0c4aeb9b5b9
BaselTurkman <Basel.Turkman123@gmail.com>
2025-03-20 18:59:13 +0200

    Handle case where note is not found during update

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
         if (!title || !content) {
             return res.status(400).json({ message: "All Fields are Required" })
         }
-        
+
         const newNode = new Note({ title, content })
         await newNode.save()
         res.status(201).json(newNode)
@@ -41,6 +41,10 @@ router.put("/:id", async (req, res) => {
         }
 
         const updateNote = await Note.findByIdAndUpdate(req.params.id, { title, content }, { new: true })
+        if (!updateNote) {
+            return res.status(404).json({ message: "Note not found" });
+        }
+
         res.status(200).json(updateNote)
     } catch (error) {
         res.status(500).json({ message: "server error!", error: error.message })
